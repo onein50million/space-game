e1c3e5d6781c101758f8f51c6374b568a48db11c
Daniel Wart <dman.wart@gmail.com>
2021-03-05 17:40:40 -0800

    added shield sounds

--- shared/ship_subsystems/shield/shield.gd
+++ shared/ship_subsystems/shield/shield.gd
@@ -1,6 +1,9 @@
 extends Subsystem
 var shield_diameter = 400
 const MINIMUM_CHARGE = 100
+
+var shield_up_before = false
+
 func _ready():
 	$shader_base.scale = Vector2(shield_diameter,shield_diameter)
 	$shield_area/collision_circle.shape.radius = shield_diameter/2.0
@@ -11,12 +14,18 @@ func _process(delta):
 	for system in get_parent().get_parent().systems:
 		if system.type == "capacitor":
 			total_charge_left += system.max_charge - system.current_charge
-	if total_charge_left < MINIMUM_CHARGE:
-		visible = false
-		$shield_area/collision_circle.set_deferred("disabled", true)
-	else:
+	var shield_up = total_charge_left > MINIMUM_CHARGE
+	
+	if shield_up and not shield_up_before:
+		$shield_up_sound.play()
 		visible = true
 		$shield_area/collision_circle.set_deferred("disabled", false)
+	elif not shield_up and shield_up_before:
+		$shield_down_sound.play()
+		visible = false
+		$shield_area/collision_circle.set_deferred("disabled", true)
+		
+	shield_up_before = shield_up
 func damage(damage):
 	var damage_left = damage
 	var ship = get_parent().get_parent()
